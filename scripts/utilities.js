54cdd5f953f861e8b0013f40d5a02bd5936c0006
ANDREW NEIDLEY <andrewneidley@ANDREWs-MacBook-Pro.local>
2017-04-30 20:50:21 -0500

    Assignment 9 completed

---
+++ scripts/utilities.js
@@ -0,0 +1,13 @@
+var pointsArray = document.getElementsByClassName ('point');
+
+var animatePoints = function(points) {
+
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+    revealPoint.forEach(animatePoints);
+};
