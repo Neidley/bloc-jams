8bc92d48e217b6883c286f22a52e06c789352261
ANDREW NEIDLEY <andrewneidley@ANDREWs-MacBook-Pro.local>
2017-05-01 05:22:04 -0500

    Assignment 9 DOM scripting events: completed

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,13 +1,5 @@
-var pointsArray = document.getElementsByClassName ('point');
-
-var animatePoints = function(points) {
-
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i]);
     }
-
-    revealPoint.forEach(animatePoints);
-};
+}
