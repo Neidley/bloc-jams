6ad023fb3c56d992de6b58328f5f09f2fdcee67c
ANDREW NEIDLEY <andrewneidley@ANDREWs-MacBook-Pro.local>
2017-05-04 17:53:55 -0500

    Assignment 13 completed

--- scripts/album.js
+++ scripts/album.js
@@ -65,6 +65,12 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+
+        if (currentParentclassName === null) {
+            console.log('No Parent Found')
+        } else (currentParent.className !== targetClass) {
+            console.log('No parent found with that class name')
+        }
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
